89bf4c41151f9b14b31cfe6f856b5b74252a4b6d
dev-nirob1 <dev.nirob1@gmail.com>
2025-08-22 15:20:16 +0600

    initialized

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import FooterSection from './components/Section/FooterSection.vue';
+import NavbarSection from './components/Section/NavbarSection.vue';
+
+</script>
+
+<template>
+  <NavbarSection/>
+  <FooterSection/>
+</template>
+
+<style scoped></style>
