a7fe047ca432e70fbb6ddd7a7acd0ceec66be765
dev-nirob1 <dev.nirob1@gmail.com>
2025-08-26 00:42:38 +0600

    update

--- src/zems/Front/Components/Section/ServicesSection.vue
+++ src/zems/Front/Components/Section/ServicesSection.vue
@@ -1,27 +1,15 @@
 <script setup lang="ts">
 import SectionTitle from '@/components/Widget/SectionTitle.vue';
+import ServiceCard from '../Widget/ServiceCard.vue';
 
 </script>
 
 <template>
   <section class="services">
     <div class="container">
-      <div class="medium-3 gap-2">
-        <SectionTitle subTitle="Our Services" title="We Are Providing Digital Services" />
-        <div class="span-2">
-          <div class="medium-2 gap-2">
-            <div v-for="b in 4" :key="b" class="services-card">
-              <div class="icon">
-                <img src="https://zems.uk/services/web-dev.png" alt="icon">
-              </div>
-              <h5>Web Develpment</h5>
-              <p>
-                Web Develpment
-                Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia
-                non.</p>
-            </div>
-          </div>
-        </div>
+      <SectionTitle subTitle="Our Services" title="We Are Providing Digital Services" />
+      <div class="medium-3 gap-2 align-center">
+          <ServiceCard v-for="s in 5" :key="s" />
       </div>
 
     </div>
@@ -33,9 +21,4 @@ import SectionTitle from '@/components/Widget/SectionTitle.vue';
   padding: 3.75rem 0;
 }
 
-.services-card {
-  padding: 2rem;
-  border-radius: 1rem;
-  background: var(--white-color);
-}
 </style>
---
+++ src/zems/Front/Components/Widget/ServiceCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="services-card">
+    <div class="icon">
+      <img src="https://zems.uk/services/web-dev.png" alt="icon">
+    </div>
+    <h5>Web Develpment</h5>
+    <p>
+      Web Develpment
+      Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia
+      non.</p>
+  </div>
+</template>
+<style>
+.services-card {
+  padding: 2rem;
+  border-radius: 1rem;
+  background: var(--white-color);
+}
+</style>
