ca9a4e7943b432d9be9b92c4c36829b432e6ab64
dev-nirob1 <dev.nirob1@gmail.com>
2025-08-26 23:00:57 +0600

    update video section

--- src/zems/Front/Components/Section/VideoSection.vue
+++ src/zems/Front/Components/Section/VideoSection.vue
@@ -1,7 +1,7 @@
 <template>
   <section>
     <div class="video relative">
-      <button class="btn"><i class="fa-solid fa-play"></i></button>
+      <button class="btn"><i class="fa-solid fa-play fa-2x"></i></button>
     </div>
   </section>
 </template>
@@ -9,9 +9,21 @@
 .video {
   max-width: 990px;
   width: 100%;
-  height: 70vh;
+  height: 85vh;
   margin: 0 auto;
-  background: url('https://www.webpoka.com/front/images/background/pattern-6.png');
+  background:url('https://www.webpoka.com/front/images/background/pattern-6.png') center / cover no-repeat, linear-gradient(to right, #EA33A6, #6538D6) ;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 1rem;
+}
+
+.video .btn {
+  border: none;
+  height: 5rem;
+  width: 5rem;
+  border-radius: 50%;
+  background: var(--white-color);
   display: flex;
   align-items: center;
   justify-content: center;
