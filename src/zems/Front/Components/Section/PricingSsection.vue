2ada4cae97baef315c76c717b8c3c20e33ca3ddf
dev-nirob1 <dev.nirob1@gmail.com>
2025-08-26 21:47:14 +0600

    update pricing widget

--- src/zems/Front/Components/Section/PricingSsection.vue
+++ src/zems/Front/Components/Section/PricingSsection.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import SectionTitle from '@/components/Widget/SectionTitle.vue';
+import PricingCard from '../Widget/PricingCard.vue';
 
 </script>
 
@@ -9,24 +10,7 @@ import SectionTitle from '@/components/Widget/SectionTitle.vue';
       <SectionTitle class="mb-1 text-center" subTitle="Best Pricing" title="Our Pricing Plans" />
 
       <div class="medium-2 large-3 gap-2">
-        <div class="pricing-card">
-          <h5>Starter Plan</h5>
-          <img src="https://webpoka.com/front/images/resource/price-icon-1.png" alt="icon">
-          <div class="price flex align-center justify-center">
-            <sup>$</sup>
-            <h1>99.9</h1>
-            <sup>/mon</sup>
-          </div>
-
-          <ul>
-            <li>Increase traffic 130%</li>
-            <li>Backlink analysis</li>
-            <li>24/7 support</li>
-            <li>10 Free Optimization</li>
-            <li>Organic traffic 215%</li>
-          </ul>
-          <button class="btn">Buy Now</button>
-        </div>
+       <PricingCard v-for="pricing in 3" :key="pricing"/>
       </div>
     </div>
   </section>
@@ -36,19 +20,4 @@ import SectionTitle from '@/components/Widget/SectionTitle.vue';
   padding: 3.75rem 0;
 }
 
-.pricing-card {
-  text-align: center;
-  padding: 3rem 1rem;
-  background: var(--white-color);
-}
-.pricing-card li{
-  margin: .5rem 0;
-}
-.pricing-card .btn {
-  padding: 1rem 2.5rem;
-  background: orange;
-  color: var(--white-color);
-  border: none;
-  border-radius: 1.5rem;
-}
 </style>
---
+++ src/zems/Front/Components/Widget/PricingCard.vue
@@ -0,0 +1,40 @@
+<template>
+   <div class="pricing-card">
+          <h5>Starter Plan</h5>
+          <img src="https://webpoka.com/front/images/resource/price-icon-1.png" alt="icon">
+          <div class="price flex align-center justify-center">
+            <sup>$</sup>
+            <h1>99.9</h1>
+            <sup>/mon</sup>
+          </div>
+
+          <ul>
+            <li>Increase traffic 130%</li>
+            <li>Backlink analysis</li>
+            <li>24/7 support</li>
+            <li>10 Free Optimization</li>
+            <li>Organic traffic 215%</li>
+          </ul>
+          <button class="btn">Buy Now</button>
+        </div>
+</template>
+<style>
+
+.pricing-card {
+  text-align: center;
+  padding: 3rem 1rem;
+  background: var(--white-color);
+}
+
+.pricing-card li {
+  margin: .5rem 0;
+}
+
+.pricing-card .btn {
+  padding: 1rem 2.5rem;
+  background: orange;
+  color: var(--white-color);
+  border: none;
+  border-radius: 1.5rem;
+}
+</style>
