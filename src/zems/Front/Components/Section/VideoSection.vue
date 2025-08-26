5a1b1a1b4b54f8b6a1227784eb4846952445c8e4
dev-nirob1 <dev.nirob1@gmail.com>
2025-08-26 21:52:20 +0600

    update video section

---
+++ src/zems/Front/Components/Section/VideoSection.vue
@@ -0,0 +1,19 @@
+<template>
+  <section>
+    <div class="video relative">
+      <button class="btn"><i class="fa-solid fa-play"></i></button>
+    </div>
+  </section>
+</template>
+<style scoped>
+.video {
+  max-width: 990px;
+  width: 100%;
+  height: 70vh;
+  margin: 0 auto;
+  background: url('https://www.webpoka.com/front/images/background/pattern-6.png');
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
--- src/zems/Front/Pages/HomePage.vue
+++ src/zems/Front/Pages/HomePage.vue
@@ -3,6 +3,7 @@ import BlogSection from '../Components/Section/BlogSection.vue';
 import HeroSection from '../Components/Section/HeroSection.vue';
 import PricingSsection from '../Components/Section/PricingSsection.vue';
 import ServicesSection from '../Components/Section/ServicesSection.vue';
+import VideoSection from '../Components/Section/VideoSection.vue';
 
 </script>
 
@@ -10,5 +11,6 @@ import ServicesSection from '../Components/Section/ServicesSection.vue';
   <HeroSection />
   <ServicesSection/>
   <BlogSection/>
+  <VideoSection/>
   <PricingSsection/>
 </template>
