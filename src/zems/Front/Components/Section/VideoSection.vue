e445084a7a48969b236a645946d1645e7ea4a3a9
dev-nirob1 <dev.nirob1@gmail.com>
2025-09-02 23:20:10 +0600

    update video section

--- src/zems/Front/Components/Section/VideoSection.vue
+++ src/zems/Front/Components/Section/VideoSection.vue
@@ -1,20 +1,33 @@
 <template>
-  <section>
-    <div class="video relative">
-      <button class="btn"><i class="fa-solid fa-play fa-2x"></i></button>
+  <section class="video relative">
+    <div class="video-play flex-center">
+      <BaseButton class="flex-center"><i class="fa-solid fa-play fa-2x"></i></BaseButton>
     </div>
   </section>
 </template>
 <style scoped>
 .video {
-  max-width: 990px;
+  padding: 3.75rem 0;
+}
+.video::before {
+  content: '';
+  position: absolute;
+  height: 70%;
+  width: 50%;
+  top: 5.5rem;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  background: url('https://www.webpoka.com/front/images/background/pattern-7.png') left no-repeat;
+  z-index: -1;
+}
+
+.video-play {
+  max-width: 950px;
   width: 100%;
-  height: 85vh;
+  height: 80vh;
   margin: 0 auto;
-  background: url('https://www.webpoka.com/front/images/background/pattern-6.png') center / cover no-repeat, linear-gradient(to right, #EA33A6, #6538D6);
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  background: url('https://www.webpoka.com/front/images/background/pattern-6.png') center  no-repeat, linear-gradient(to right, #EA33A6, #6538D6);
   border-radius: 1rem;
 }
 
@@ -24,8 +37,6 @@
   width: 5rem;
   border-radius: 50%;
   background: var(--white-color);
-  display: flex;
-  align-items: center;
-  justify-content: center;
 }
+
 </style>
--- src/zems/Front/Pages/HomePage.vue
+++ src/zems/Front/Pages/HomePage.vue
@@ -1,16 +1,17 @@
 <script setup>
 import HeroSection from '../Components/Section/HeroSection.vue';
 import ServicesSection from '../Components/Section/ServicesSection.vue';
+import VideoSection from '../Components/Section/VideoSection.vue';
 import WhyChooseSection from '../Components/Section/WhyChooseSection.vue';
 
 </script>
 
 <template>
   <HeroSection />
-  <ServicesSection/>
-  <WhyChooseSection/>
-  <!-- <VideoSection/>
-  <ProcesSection/>
+  <ServicesSection />
+  <WhyChooseSection />
+  <VideoSection />
+  <!-- <ProcesSection/>
   <PricingSsection/>
   <AboutSection/>
   <BlogSection/> -->
