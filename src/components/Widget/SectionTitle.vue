369d2a370fa1c47df6381af9c83395d10769c66e
dev-nirob1 <dev.nirob1@gmail.com>
2025-09-11 15:59:16 +0600

    update section title

--- src/components/Widget/SectionTitle.vue
+++ src/components/Widget/SectionTitle.vue
@@ -11,14 +11,14 @@ defineProps({
 
 <template>
   <div class="section-title">
-    <h5>{{ subTitle }}</h5>
+    <SubTitle>{{ subTitle }}</SubTitle>
     <span> </span>
 
-    <h3>{{ title }}</h3>
+    <BaseTitle>{{ title }}</BaseTitle>
   </div>
 </template>
 <style>
-.section-title h5 {
+.section-title .sub-title {
   color: var(--secondary-color);
 }
 
@@ -32,23 +32,22 @@ defineProps({
   background: var(--secondary-color);
 }
 
-.section-title span::after {
+.section-title span::after,
+.section-title span::before {
   content: '';
   position: absolute;
-  top: 1px;
-  left: -1rem;
   height: 2px;
   width: 10px;
   background: var(--alternative-color);
 }
 
+.section-title span::after {
+  top: 1px;
+  left: -1rem;
+}
+
 .section-title span::before {
-  content: '';
-  position: absolute;
   top: 1px;
   left: 1rem;
-  height: 2px;
-  width: 10px;
-  background: var(--alternative-color);
 }
 </style>
