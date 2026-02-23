1920d4ea6c43684b8a451b2218c5b1309a765934
dev-nirob1 <dev.nirob1@gmail.com>
2026-02-23 23:02:43 +0600

    logo update

--- src/components/Section/NavbarSection.vue
+++ src/components/Section/NavbarSection.vue
@@ -34,7 +34,11 @@ onMounted(() => {
     <nav class="flex justify-between align-center container">
       <!-- Logo -->
       <RouterLink to="/" class="logo">
-        <BaseImage class="width-full" image="/public/logo.jpeg" alt="logo" />
+        <BaseImage class="width-full" image="/webpoka.png" alt="logo" />
+        <div>
+          <SubTitle>Webpoka</SubTitle>
+          <BaseParagraph>Reliable Software Company</BaseParagraph>
+        </div>
       </RouterLink>
 
       <!-- Navigation Links -->
@@ -87,7 +91,7 @@ onMounted(() => {
 }
 
 .navbar {
-  padding: 0.5rem 0;
+  padding: 1.5rem 0;
   position: fixed;
   top: 0;
   left: 0;
@@ -100,9 +104,22 @@ onMounted(() => {
 .navbar a {
   text-decoration: none;
 }
-
+.logo {
+  display: flex;
+  align-items: center;
+}
+.logo p {
+  margin: 0;
+  font-size: .77rem;
+}
+.logo .sub-title{
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  font-size: 1.75rem;
+  line-height: 1;
+}
 .logo img {
-  height: 50px;
+  height: 60px;
   width: auto;
   display: block;
 }
